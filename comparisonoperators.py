fa416d4da6ca2dfc5039866237ef918229393c7d
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-03-29 19:11:06 +0530

    Add files via upload

---
+++ comparisonoperators.py
@@ -0,0 +1,11 @@
+t = int(input("Enter the temperature: "))
+
+if t > 30:
+    print("It's a hot day.")
+
+elif t < 30:
+    print("It's a cold day.")
+    
+else:
+    print("It's a normal day.")
+    
