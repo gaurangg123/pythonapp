41dc38201bcc1e294c82af2ae505596631050f76
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-03-31 01:32:28 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,17 @@
+for item in 'Python':
+    print(item)
+    
+for item in ['Mosh', 'John', 'Seria']:
+    print(item)
+    
+for item in [1,2,3,4]:
+    print(item)
+    
+for item in range(10):
+    print(item)
+
+for item in range(5,10):
+    print(item)
+
+for item in range(5, 10, 2):
+    print(item)
