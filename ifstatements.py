8bd16f6909d097a11ea7f53d3207cb3733027110
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-04-04 00:07:30 +0530

    Add files via upload

---
+++ ifstatements.py
@@ -0,0 +1,15 @@
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("\nIt's a Hot Day!")
+    print("Drink plenty of water.")
+    
+elif is_cold:
+    print("It's a cold day!")
+    print("Wear warm clothes")
+    
+else:
+    print("Enjoy Your Day as it is a lovely day!!")
+
+print("Enjoy Your Day as it is a lovely day!!")
