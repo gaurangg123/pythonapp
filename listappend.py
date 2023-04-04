3c5e2b318d48e98d16e71309be3ea61b3d75df5c
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-04-04 11:36:34 +0530

    Add files via upload

---
+++ listappend.py
@@ -0,0 +1,18 @@
+numbers = [5,2,1,7,4]
+numbers.append(20)
+print(numbers)
+
+numbers.insert(0,10)
+print(numbers)
+
+numbers.remove(5)
+print(numbers)
+
+# numbers.clear()
+# print(numbers)
+
+numbers.pop()
+print(numbers)
+
+numbers.count()
+print(numbers)
