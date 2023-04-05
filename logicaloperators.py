1a94d9b6c5947b40cd40bf26e9a17d1878db2e83
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-04-06 00:01:23 +0530

    Add files via upload

---
+++ logicaloperators.py
@@ -0,0 +1,9 @@
+# has_high_income = True
+has_good_credit = True
+has_criminal_record = True
+    
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+
+#AND - both conditions will be true
+#OR - at least one condition should be true
