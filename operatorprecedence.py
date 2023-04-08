de1e3f0ed2c6f34afd40dcbe01b76e816033d30d
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-04-08 15:49:37 +0530

    Add files via upload

---
+++ operatorprecedence.py
@@ -0,0 +1,11 @@
+x = (10 + 3) * 2 ** 2
+print(x)
+
+#order of operations 
+#parenthesis
+#exponentation 2 ** 3
+#multiplication or division 
+#addition or subtraction
+
+x = (2+3) * 10 - 3
+print(x)
