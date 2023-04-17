3d00d0c98eba2c978c54dfaaa9aaa55d7c3a6455
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-04-17 17:33:57 +0530

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,8 @@
+course = "Python for Begineers"
+print(len(course))
+
+course_1 = "Python for Begineers"
+print(course_1.upper())
+print(course_1.lower())
+print(course_1.find('P'))
+print(course_1.replace('Begineers', 'Absolute Beginners'))
