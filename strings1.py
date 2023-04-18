151e4e85145830c43dacf6eef6da2b7efef8fc40
Gaurang Ashava <98259190+gaurangg123@users.noreply.github.com>
2023-04-18 23:19:57 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,15 @@
+course = "Python's course for begineers"
+print(course)
+
+course_1 = 'Python for "Begineers"'
+print(course_1)
+
+course_2 = 'Python for "Begineers"'
+print(course_2[-4])
+
+course_3 = 'Python for "Begineers"'
+another = course_3 [:]
+print(another)
+
+name = 'jennifer'
+print(name[1:-1])
